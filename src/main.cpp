f788f325fdd3e31d527ae9514b6726a3f55b6949
levshx <pavellevshic@gmail.com>
2023-07-12 12:51:54 +0300

    kek

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <windows.h>
+#include "../lib/vendor/glad/include/glad/glad.h"
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    cout<<"PROGRAM VERSION 0.1"<<endl;
+
+    setlocale(0, "");
+
+    int a, b;
+    cout<<"enter number a: ";
+    cin>>a;
+    cout<<"enter number b: ";
+    cin>>b;
+    if (a+b>10) {
+        cout<<"BIG";
+    }
+    else {
+        cout<<"little";
+    }
+
+    
+
+    return 0;
+}
